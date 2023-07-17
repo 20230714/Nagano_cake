b037e4dbf84bb56802bc81be0625996a715eb35d
JunyaShibata <135484706+junyashibata@users.noreply.github.com>
2023-07-17 04:01:17 +0000

    add admin_kaiinnjohohensyu

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -11,5 +11,24 @@ class Admin::CustomersController < ApplicationController
   end
   
   def edit
+    #publicで作成したら＃外す。（しばじゅん）
+    #@customer = Customer.find(params[:id])
   end
+  
+  def update
+    #publicで作成したら＃外す。（しばじゅん）
+    #@customer = Customer.find(params[:id])
+    #if @customer.update(customer_params)
+       #flash[:notice] = "会員情報を更新しました。"
+       #redirect_to request.referer
+    #else
+      #render "edit"
+    #end
+  end
+  
+  private
+  
+   def customer_params
+     params.require(:customer).permit(:first_name,:last_name,:kana_first_name,:kana_last_name,:postal_code,:address,:phone_number,:email,:is_deleted)
+   end
 end
