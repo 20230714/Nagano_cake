94bdbb483ee5dbcc820a2454bb789521268b2545
EC2 Default User <ec2-user@ip-172-31-35-33.ap-northeast-1.compute.internal>
2023-07-18 12:48:59 +0000

    add genre_search_function

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,8 +2,13 @@ class Public::ItemsController < ApplicationController
   before_action :authenticate_customer!, except: [:index, :show]
 
   def index
-    @items = Item.all.page(params[:page]).per(8)
     @genres = Genre.all
+  if params[:genre_id]
+  	genre = Genre.find(params[:genre_id])
+  	@items = genre.items.page(params[:page]).per(8)
+  else
+  	@items = Item.all.page(params[:page]).per(8)
+  end
   end
 
   def show
