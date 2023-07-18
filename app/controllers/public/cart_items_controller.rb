cb6b6b2c3561505c3adc4f783b82dbba51e0008c
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-07-18 00:50:16 +0000

    add cart-item,view

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,8 +1,9 @@
 class Public::CartItemsController < ApplicationController
-
+  before_action :authenticate_customer!
 
   def index
-
+  @cart_items = current_customer.cart_items
+  @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
   end
 
   def update
@@ -18,7 +19,13 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+  @cart_item = CartItem.new(cart_items_params)
 
+  end
+
+private
+  def cart_items_params
+    params.require(:cart_item).permit(:customer_id, :item_id, :quantity)
   end
 
 end
