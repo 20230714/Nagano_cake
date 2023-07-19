bef2a72c150909324c0dd96b178b8c06868e8b4e
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-07-19 05:58:36 +0000

    add order-new-view,controller, enum, enum-ja

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,10 +1,32 @@
 class Public::OrdersController < ApplicationController
+before_action :authenticate_customer!
 
   def new
-
+  @order = Order.new
+  @addresses = current_customer.addresses.all
   end
 
   def confirm
+    @order = Order.new(order_params)
+
+    if params[:order][:address_option] == "0"
+        @order.postcode = current_customer.postcode
+        @order.address = current_customer.address
+        @order.name = current_customer.last_name + current_customer.first_name
+
+    elsif params[:order][:address_option] == "1"
+        ship = Address.find(params[:order][:customer_id])
+        @order.postcode = ship.post_code
+        @order.address = ship.address
+        @order.name = ship.name
+
+    elsif params[:order][:address_option] = "2"
+        @order.postcode = params[:order][:postcode]
+        @order.address = params[:order][:address]
+        @order.name = params[:order][:name]
+    else
+        render 'new'
+    end
 
   end
 
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,6 +6,7 @@ class Customer < ApplicationRecord
 
   has_many :orders, dependent: :destroy
   has_many :cart_items, dependent: :destroy
+  has_many :addresses
 
   # 会員フルネーム(しばじゅん）
   def full_name
