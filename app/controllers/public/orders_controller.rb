4e7366c21bbccc529586fd8604772b469b4933cc
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-07-19 09:58:27 +0000

    add order-confirm-view,controller

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,17 +8,18 @@ before_action :authenticate_customer!
 
   def confirm
     @order = Order.new(order_params)
-
+    @cart_items = current_customer.cart_items.all
+    @total = 0
     if params[:order][:address_option] == "0"
         @order.postcode = current_customer.postcode
         @order.address = current_customer.address
         @order.name = current_customer.last_name + current_customer.first_name
 
     elsif params[:order][:address_option] == "1"
-        ship = Address.find(params[:order][:customer_id])
-        @order.postcode = ship.post_code
-        @order.address = ship.address
-        @order.name = ship.name
+        @address = Address.find(params[:order][:address_id])
+        @order.postcode = @address.postcode
+        @order.address = @address.address
+        @order.name = @address.name
 
     elsif params[:order][:address_option] = "2"
         @order.postcode = params[:order][:postcode]
@@ -35,8 +36,28 @@ before_action :authenticate_customer!
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.save
 
+    if params[:order][:ship] == "1"
+      current_customer.address.create(address_params)
+    end
+
+    @cart_items = current_customer.cart_items
+    @cart_items.each do |cart_item|
+      @order_detail = OrderDetail.new
+      @order_detail.item_id = cart_item.item_id
+      @order_detail.order_id = @order.id
+      @order_detail.quantity = cart_item.quantity
+      @order_detail.price = cart_item.item.price * cart_item.quantity
+      @order_detail.save
+      end
+
+    @cart_items.destroy_all
+
+    redirect_to complete_orders_path
   end
+
 
   def index
 
@@ -49,10 +70,6 @@ before_action :authenticate_customer!
   private
   def order_params
     params.require(:order).permit(:postcode, :address, :name, :payment_method)
-  end
-
-  def address_params
-    params.require(:order).permit(:postcode, :address, :name)
   end
 
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :customer
-  
+
   def address_display
-  '〒' + postal_code + ' ' + address + ' ' + name
+  '〒' + postcode + ' ' + address + ' ' + name
   end
 end
