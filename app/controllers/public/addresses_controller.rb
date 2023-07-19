cec949c291cef5dc0eb24da00aec4ede8d177bd4
JunyaShibata <135484706+junyashibata@users.noreply.github.com>
2023-07-19 00:22:15 +0000

    Add edit address

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -3,32 +3,32 @@ class Public::AddressesController < ApplicationController
     @address = Address.new
     @addresses = Address.all
   end
-  
+
   def create
     @address = Address.new(address_params)
     @address.customer_id = current_customer.id
     @address.save
     redirect_to addresses_path
   end
-  
+
   def destroy
     @address = Address.find(params[:id])
     @address.destroy
     redirect_to addresses_path
   end
-  
+
   def edit
     @address = Address.find(params[:id])
   end
-  
+
   def update
     @address = Address.find(params[:id])
     @address.update(address_params)
-    redirect_to address_path
+    redirect_to addresses_path
   end
-  
+
   private
-  
+
   def address_params
     params.require(:address).permit(:postcode, :address, :name)
   end
