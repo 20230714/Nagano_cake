bfd5c1cd2f8e3abada7c01ae36d0271a189e7f81
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-07-21 05:30:52 +0000

    add order-view

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,8 +1,15 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
-  
+
   def subtotal
     item.add_tax_price*quantity
   end
+
+  def self.ordertotal
+    sum(:quantity)
+  end
+
+  enum making_status: {"着手不可": 0,"製作待ち": 1,"製作中": 2,"製作完了": 3}
+
 end
