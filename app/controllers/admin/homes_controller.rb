a03e8e1d5d7bb71b15493854a4af2482c5bb4f73
EC2 Default User <ec2-user@ip-172-31-35-33.ap-northeast-1.compute.internal>
2023-07-21 08:46:10 +0000

    add customer_order_view

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,7 +1,12 @@
 class Admin::HomesController < ApplicationController
 
   def top
-    @orders = Order.page(params[:page]).per(10)
+    if params[:customer_id]
+      @customer = Customer.find(params[:customer_id])
+      @orders = @customer.orders.page(params[:page]).per(10)
+    else
+      @orders = Order.page(params[:page]).per(10)
+    end
   end
 
   private
